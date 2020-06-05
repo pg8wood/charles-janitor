c9a157605d980dae0f40f0fd71949c6c750ddebf
Patrick Gatewood <pg8wood@vt.edu>
2020-06-05 15:27:08 -0400

    Initial Commit

---
+++ CharlesProxyWrapperTests/CharlesProxyWrapperTests.swift
@@ -0,0 +1,34 @@
+//
+//  CharlesProxyWrapperTests.swift
+//  CharlesProxyWrapperTests
+//
+//  Created by Patrick Gatewood on 6/5/20.
+//  Copyright © 2020 Patrick Gatewood. All rights reserved.
+//
+
+import XCTest
+@testable import CharlesProxyWrapper
+
+class CharlesProxyWrapperTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
