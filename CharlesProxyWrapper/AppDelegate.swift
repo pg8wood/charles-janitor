f74957874d4df06f2d08a9758888d55343664dd5
Patrick Gatewood <pg8wood@vt.edu>
2020-06-05 17:15:24 -0400

    Initial commit

--- CharlesProxyWrapper/AppDelegate.swift
+++ CharlesProxyWrapper/AppDelegate.swift
@@ -11,29 +11,72 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    private let charlesBundleID = "com.xk72.Charles"
+    
+    private var charlesProcessObserver: NSKeyValueObservation?
+    private var charlesInstance: NSRunningApplication?
+    
     var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+        
+        guard let charlesURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: charlesBundleID) else {
+            // TODO let user pick their install location or show error
+            print("Couldn't find Charles install")
+            return
+        }
+
+        
+        NSWorkspace.shared.openApplication(at: charlesURL, configuration: .init()) { [weak self] charlesInstance, error in
+            guard let self = self else { return }
+            
+            guard let charlesInstance = charlesInstance,
+                error == nil else {
+                print("failed to open charles")
+                return
+            }
+            
+            print("got charles instance with pid: \(charlesInstance.processIdentifier), isTerminated: \(charlesInstance.isTerminated)")
+            self.charlesInstance = charlesInstance
+            self.charlesProcessObserver = charlesInstance.observe(\.isTerminated, changeHandler: self.observedApplicationIsTerminatedChange(_:isTerminated:))
+        }
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+//        let contentView = ContentView()
+//
+//        // Create the window and set the content view.
+//        window = NSWindow(
+//            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+//            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+//            backing: .buffered, defer: false)
+//        window.center()
+//        window.setFrameAutosaveName("Main Window")
+//        window.contentView = NSHostingView(rootView: contentView)
+//        window.makeKeyAndOrderFront(nil)
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    private func observedApplicationIsTerminatedChange(_ application: NSRunningApplication, isTerminated: NSKeyValueObservedChange<Bool>) {
+        func disableProxies() {
+            let webProxyOutput = ProcessRunner.shell("/usr/sbin/networksetup", ["-setwebproxystate", "Wi-Fi", "off"])
+            let secureWebProxyOutput = ProcessRunner.shell("/usr/sbin/networksetup", ["-setsecurewebproxystate", "Wi-Fi", "off"])
+            
+            if let errorMessage = [webProxyOutput, secureWebProxyOutput]
+                .compactMap({ $0 })
+                .filter({ !$0.isEmpty })
+                .first {
+                print("error deactivating proxies: \(errorMessage)")
+            }
+        }
+        
+        guard let charlesInstance = self.charlesInstance ??
+            NSRunningApplication.runningApplications(withBundleIdentifier: charlesBundleID).first else {
+                disableProxies()
+                return
+        }
+        
+        if charlesInstance.isTerminated {
+            disableProxies()
+        }
     }
-
-
 }
-
---
+++ CharlesProxyWrapper/ProcessRunner.swift
@@ -0,0 +1,39 @@
+//
+//  ProcessRunner.swift
+//  CharlesProxyWrapper
+//
+//  Created by Patrick Gatewood on 6/5/20.
+//  Copyright © 2020 Patrick Gatewood. All rights reserved.
+//
+
+import Foundation
+
+class ProcessRunner {
+    /**
+     Runs a subprocess and returns its output
+     
+     - parameter launchPath: The executable to run
+     - parameter args: Arguments to pass to the executable
+     
+     - returns: An optional String with contents of the process' standard output
+     */
+    @discardableResult
+    static func shell(_ launchPath: String, _ args: [String]) -> String? {
+        let process = Process()
+        process.launchPath = launchPath
+        process.arguments = args
+        
+        // Capture the process' output
+        let pipe = Pipe()
+        process.standardOutput = pipe
+        
+        // Run the process
+        process.launch()
+        process.waitUntilExit()
+        
+        if let output = String(data: pipe.fileHandleForReading.readDataToEndOfFile(),  encoding: String.Encoding.utf8) {
+            return output
+        }
+        return nil
+    }
+}
