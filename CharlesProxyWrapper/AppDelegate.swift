809efd86b938f4aa942c1e8cc4cb75120d995870
Patrick Gatewood <pg8wood@vt.edu>
2020-06-09 17:18:42 -0400

    Reorganize CharlesRunner
    Encapsulate Charles-related tasks and move them out of the
    AppDelegate.

--- CharlesProxyWrapper/AppDelegate.swift
+++ CharlesProxyWrapper/AppDelegate.swift
@@ -11,65 +11,10 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    private let charlesBundleID = "com.xk72.Charles"
-    
-    private var charlesProcessObserver: NSKeyValueObservation?
-    private var charlesInstance: NSRunningApplication?
-    
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         NSApp.setActivationPolicy(.accessory)
         
-        guard let charlesURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: charlesBundleID) else {
-            AlertController.presentMissingCharlesAlert()
-            return
-        }
-        
-        NSWorkspace.shared.openApplication(at: charlesURL, configuration: .init()) { [weak self] charlesInstance, error in
-            guard let self = self else { return }
-            
-            guard let charlesInstance = charlesInstance,
-                error == nil else {
-                    DispatchQueue.main.async {
-                        AlertController.showBugAlert(
-                            title: "Failed to open Charles.",
-                            message: error?.localizedDescription ?? "Unknown error. 🤷‍♀️ \n\nIf you think this is a bug, please report it on GitHub.",
-                            style: .critical,
-                            terminateOnClose: true)
-                    }
-                    return
-            }
-            
-            self.charlesInstance = charlesInstance
-            self.charlesProcessObserver = charlesInstance.observe(\.isTerminated, changeHandler: self.observedApplicationIsTerminatedChange(_:isTerminated:))
-        }
-    }
-    
-    private func observedApplicationIsTerminatedChange(_ application: NSRunningApplication, isTerminated: NSKeyValueObservedChange<Bool>) {
-        func disableProxies() {
-            defer {
-                NSApp.terminate(self)
-            }
-            
-            let webProxyOutput = ProcessRunner.shell("/usr/sbin/networksetup", ["-setwebproxystate", "Wi-Fi", "off"])
-            let secureWebProxyOutput = ProcessRunner.shell("/usr/sbin/networksetup", ["-setsecurewebproxystate", "Wi-Fi", "off"])
-            
-            if let _ = [webProxyOutput, secureWebProxyOutput]
-                .compactMap({ $0 })
-                .filter({ !$0.isEmpty })
-                .first {
-                AlertController.showBugAlert(title: "An error occurred disabling Wi-Fi proxies.", message: "Please report this bug on GitHub.")
-            }
-        }
-        
-        guard let charlesInstance = self.charlesInstance ??
-            NSRunningApplication.runningApplications(withBundleIdentifier: charlesBundleID).first else {
-                disableProxies()
-                return
-        }
-        
-        if charlesInstance.isTerminated {
-            disableProxies()
-        }
+        CharlesRunner.runCharles()
     }
 }
---
+++ CharlesProxyWrapper/CharlesRunner.swift
@@ -0,0 +1,67 @@
+//
+//  CharlesRunner.swift
+//  Charles Janitor
+//
+//  Created by Patrick Gatewood on 6/9/20.
+//  Copyright © 2020 Patrick Gatewood. All rights reserved.
+//
+
+import AppKit
+
+struct CharlesRunner {
+    private static let charlesBundleID = "com.xk72.Charles"
+    
+    private static var charlesProcessObserver: NSKeyValueObservation?
+    private static var charlesInstance: NSRunningApplication?
+    
+    static func runCharles() {
+        guard let charlesURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: charlesBundleID) else {
+            AlertController.presentMissingCharlesAlert()
+            return
+        }
+        
+        NSWorkspace.shared.openApplication(at: charlesURL, configuration: .init()) { charlesInstance, error in            
+            guard let charlesInstance = charlesInstance, error == nil else {
+                DispatchQueue.main.async {
+                    AlertController.showBugAlert(
+                        title: "Failed to open Charles.",
+                        message: error?.localizedDescription ?? "Unknown error. 🤷‍♀️ \n\nIf you think this is a bug, please report it on GitHub.",
+                        style: .critical,
+                        terminateOnClose: true)
+                }
+                return
+            }
+            
+            self.charlesInstance = charlesInstance
+            self.charlesProcessObserver = charlesInstance.observe(\.isTerminated, changeHandler: self.observedApplicationIsTerminatedChange(_:isTerminated:))
+        }
+    }
+    
+    private static func observedApplicationIsTerminatedChange(_ application: NSRunningApplication, isTerminated: NSKeyValueObservedChange<Bool>) {
+        func disableProxies() {
+            defer {
+                NSApp.terminate(self)
+            }
+            
+            let webProxyOutput = ProcessRunner.shell("/usr/sbin/networksetup", ["-setwebproxystate", "Wi-Fi", "off"])
+            let secureWebProxyOutput = ProcessRunner.shell("/usr/sbin/networksetup", ["-setsecurewebproxystate", "Wi-Fi", "off"])
+            
+            if let _ = [webProxyOutput, secureWebProxyOutput]
+                .compactMap({ $0 })
+                .filter({ !$0.isEmpty })
+                .first {
+                AlertController.showBugAlert(title: "An error occurred disabling Wi-Fi proxies.", message: "Please report this bug on GitHub.")
+            }
+        }
+        
+        guard let charlesInstance = self.charlesInstance ??
+            NSRunningApplication.runningApplications(withBundleIdentifier: charlesBundleID).first else {
+                disableProxies()
+                return
+        }
+        
+        if charlesInstance.isTerminated {
+            disableProxies()
+        }
+    }
+}
